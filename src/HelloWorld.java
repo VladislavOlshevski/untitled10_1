285c7b8c91e6a91f1563ae05f5e28f0e1b163457
VladislavOlshevski <olshevski2.18@gmail.com>
2019-07-01 21:39:25 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by re-training on 01.07.2019.
+ */
+public class HelloWorld  {
+    public static void main(String[] args) {
+        //Display "Hello World"
+
+        //
+        System.out.println("Hello World!");
+    }
+}
